f5641e4ff68d0bcbfaeed3974893fc3df22f7011
Konrad Nölle <konrad@familie-noelle.de>
2023-02-17 13:35:10 +0100

    Make layer keys tap-toggle

--- keyboards/reasonable_excuse/keymaps/default/keymap.c
+++ keyboards/reasonable_excuse/keymaps/default/keymap.c
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,      KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,
         KC_A,    HMC_R,   HMC_S,   HMC_T,   HMC_G,     HMC_M,   HMC_N,   HMC_E,   HMC_I,   KC_O,
         KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,      KC_K,    KC_H,    KC_COMM, KC_DOT,  DF(L_QWE),
-                   KC_ESC, LGUI_T(KC_ENT), KC_BSPC,  KC_SPC,  MO(L_NAV), MO(L_SYM)
+                   KC_ESC, LGUI_T(KC_ENT), KC_BSPC,  KC_SPC,  TT(L_NAV), TT(L_SYM)
     ),
 
      /*
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
         KC_A,    HMQ_S,   HMQ_D,   HMQ_F,   HMQ_G,     HMQ_H,   HMQ_J,   HMQ_K,   HMQ_L,   KC_COLN,
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      KC_N,    KC_M,    KC_COMM, KC_DOT,  DF(L_GME),
-                   KC_ESC, LGUI_T(KC_ENT), KC_BSPC,  KC_SPC,  MO(L_NAV), MO(L_SYM)
+                   KC_ESC, LGUI_T(KC_ENT), KC_BSPC,  KC_SPC,  TT(L_NAV), TT(L_SYM)
     ),
     [L_SYM] = LAYOUT_split_3x5_3(
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
