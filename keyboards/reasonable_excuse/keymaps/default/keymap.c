464f3148f302c45fb4e22aba24ea34e919fc3e17
Konrad Nölle <konrad@familie-noelle.de>
2023-03-06 01:05:21 +0100

    Add shift-backspace -> delete key override

--- keyboards/reasonable_excuse/keymaps/default/keymap.c
+++ keyboards/reasonable_excuse/keymaps/default/keymap.c
@@ -125,6 +125,18 @@ bool get_combo_must_tap(uint16_t index, combo_t *combo) {
 }
 
 // ==========================
+// Key overrides
+// ==========================
+
+const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
+
+// This globally defines all key overrides to be used
+const key_override_t **key_overrides = (const key_override_t *[]){
+    &delete_key_override,
+    NULL // Null terminate the array of overrides!
+};
+
+// ==========================
 // "Settings"
 // ==========================
 
--- keyboards/reasonable_excuse/rules.mk
+++ keyboards/reasonable_excuse/rules.mk
@@ -1,5 +1,6 @@
 # This file intentionally left blank
 SPLIT_KEYBOARD = yes
 COMBO_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
 
 VPATH += keyboards/gboards
